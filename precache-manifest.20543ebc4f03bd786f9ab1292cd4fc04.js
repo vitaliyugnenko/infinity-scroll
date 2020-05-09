b3e5719ebc33a202e1d91cd5f720f387c3ccb160
vitaliyugnenko <blackboy2010@ukr.net>
2020-05-09 13:34:34 +0300

    Updates

---
+++ precache-manifest.20543ebc4f03bd786f9ab1292cd4fc04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3938a37a9367dd5b1456bf21004d301c",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "4481e1dffbf1e7f6024a",
+    "url": "/build/static/css/main.d869b563.chunk.css"
+  },
+  {
+    "revision": "18c2cdbcf763950ef185",
+    "url": "/build/static/js/2.edab4557.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/build/static/js/2.edab4557.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4481e1dffbf1e7f6024a",
+    "url": "/build/static/js/main.80caa09c.chunk.js"
+  },
+  {
+    "revision": "3a15ae638bd08ec3b8a7",
+    "url": "/build/static/js/runtime-main.9a21e8f3.js"
+  }
+]);
